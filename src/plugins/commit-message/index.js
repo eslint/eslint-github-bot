279329daeb5782c692fe35ae1dac4be81629db85
Teddy Katz <teddy.katz@gmail.com>
2017-10-16 15:53:39 -0400

    Update: commit message checker to use status checks (fixes #11) (#17)
    (refs https://github.com/eslint/eslint/issues/8633#issuecomment-336941799)

--- src/plugins/commit-message/index.js
+++ src/plugins/commit-message/index.js
@@ -1,68 +1,21 @@
 /**
- * @fileoverview  It checks the first commit message on PR and
- * make sure it is formatted according to the guidelines. If not then it leaves a message on the PR.
+ * @fileoverview Validates that commit messages are formatted according
+ * to the guidelines.
  * @author Gyandeep Singh
  */
 
 const TAG_REGEX = /^(?:Breaking|Build|Chore|Docs|Fix|New|Update|Upgrade):/;
-const commitMessageIssue = "* The commit summary needs to begin with a tag (such as `Fix:` or `Update:`). Please check out our [guide](http://eslint.org/docs/developer-guide/contributing/pull-requests#step-2-make-your-changes) for how to properly format your commit summary and [update](http://eslint.org/docs/developer-guide/contributing/pull-requests#updating-the-commit-message) it on this pull request.";
-const commitLengthIssue = "* The commit summary must be 72 characters or shorter. Please check out our [guide](http://eslint.org/docs/developer-guide/contributing/pull-requests#step-2-make-your-changes) for how to properly format your commit summary and [update](http://eslint.org/docs/developer-guide/contributing/pull-requests#updating-the-commit-message) it on this pull request.";
-const messageLengthLimit = 72;
-
-/**
- * Create the comment for the commit message issue
- * @param {string} username - user to tag on the comment
- * @param {Array<string>} messageIssues - collection of issue messages
- * @returns {string} comment message
- * @private
- */
-const commentMessage = (username, messageIssues) => `
-Thanks for the pull request, @${username}! I took a look to make sure it's ready for merging and found some changes are needed:
-
-${messageIssues.join("\n")}
-
-Can you please update the pull request to address these?
-(More information can be found in our [pull request guide](http://eslint.org/docs/developer-guide/contributing/pull-requests).)
-
-[//]: # (commit-message)
-`;
-
-/**
- * Check if the commit message follow the guidelines
- * @param {string} message - commit message
- * @returns {boolean} True if its ok
- * @private
- */
-const isCommitMessageLegal = (message) => TAG_REGEX.test(message);
-
-/**
- * Check if the commit message length is correct
- * @param {string} message - commit message
- * @returns {boolean} True if its ok
- * @private
- */
-const isCommitMessageLengthLegal = (message) => message.length <= messageLengthLimit;
+const MESSAGE_LENGTH_LIMIT = 72;
 
 /**
  * Apply different checks on the commit message
  * @param {string} message - commit message
- * @returns {Array<string>} Collection of issue messages
+ * @returns {boolean} `true` if the commit message is valid
  * @private
  */
-const checkCommitMessage = (message) => {
-    const issueMessages = [];
-
-    if (!isCommitMessageLegal(message)) {
-        issueMessages.push(commitMessageIssue);
-    }
-
-    if (!isCommitMessageLengthLegal(message)) {
-        issueMessages.push(commitLengthIssue);
-    }
-
-    return issueMessages;
-};
-
+function checkCommitMessage(message) {
+    return TAG_REGEX.test(message) && message.split(/\r?\n/)[0].length <= MESSAGE_LENGTH_LIMIT;
+}
 
 /**
  * If the first commit message is not legal then it adds a comment
@@ -71,16 +24,43 @@ const checkCommitMessage = (message) => {
  * @private
  */
 const processCommitMessage = async (context) => {
+    /*
+     * We care about the default commit message that will appear when the
+     * PR is merged. If the PR has exactly one commit, this is the commit
+     * message of that commit. If the PR has more than one commit, this
+     * is the title of the PR.
+     */
     const { payload, github } = context;
     const allCommits = await github.pullRequests.getCommits(context.issue());
-    const messageIssues = checkCommitMessage(allCommits.data[0].commit.message);
+    const messageToCheck = allCommits.data.length === 1
+        ? allCommits.data[0].commit.message
+        : payload.pull_request.title;
+    const isValid = checkCommitMessage(messageToCheck);
+    let description;
+    let state;
+
+    if (isValid) {
+        state = "success";
+        description = allCommits.data.length === 1
+            ? "Commit message follows guidelines"
+            : "PR title follows commit message guidelines";
+    } else {
+        state = "failure";
+        description = allCommits.data.length === 1
+            ? "Commit message doesn't follow guidelines"
+            : "PR title doesn't follow commit message guidelines";
+    }
 
     // only check first commit message
-    if (messageIssues.length > 0) {
-        github.issues.createComment(context.issue({
-            body: commentMessage(payload.sender.login, messageIssues)
-        }));
-    }
+    return github.repos.createStatus(
+        context.repo({
+            sha: allCommits.data[allCommits.data.length - 1].sha,
+            state,
+            target_url: "https://eslint.org/docs/developer-guide/contributing/pull-requests#step-2-make-your-changes",
+            description,
+            context: "commit-message"
+        })
+    );
 };
 
 /**
@@ -90,4 +70,5 @@ module.exports = (robot) => {
     robot.on("pull_request.opened", processCommitMessage);
     robot.on("pull_request.reopened", processCommitMessage);
     robot.on("pull_request.synchronize", processCommitMessage);
+    robot.on("pull_request.edited", processCommitMessage);
 };
