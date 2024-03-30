1030daf435e04cb2a7ee851ff20069aa15274556
Percy Ma <kecrily@gmail.com>
2024-03-31 01:33:46 +0800

    refactor: use flat config for eslint (#201)
    * refactor: use flat config for eslint
    
    * chore: update
    
    * Apply suggestions from code review
    
    Co-authored-by: Milos Djermanovic <milos.djermanovic@gmail.com>
    
    * Update eslint.config.js
    
    Co-authored-by: Milos Djermanovic <milos.djermanovic@gmail.com>
    
    ---------
    
    Co-authored-by: Milos Djermanovic <milos.djermanovic@gmail.com>

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const eslintConfigESLint = require("eslint-config-eslint/cjs");
+const globals = require("globals");
+
+module.exports = [
+    ...eslintConfigESLint,
+    {
+        ignores: ["coverage/"]
+    },
+    {
+        rules: {
+            camelcase: ["error", { properties: "never" }],
+
+            // Remove after https://github.com/eslint/eslint/pull/17900 is included in new release
+            "jsdoc/no-multi-asterisks": ["error", { allowWhitespace: true }]
+        }
+    },
+    {
+        files: ["eslint.config.js", "tests/**/*.js"],
+        languageOptions: {
+            globals: {
+                ...globals.jest
+            }
+        },
+        rules: {
+            "n/no-unpublished-require": "off",
+            "n/no-extraneous-require": [
+                "error",
+                {
+                    allowModules: [
+                        "@octokit/rest"
+                    ]
+                }
+            ]
+        }
+    }
+];
