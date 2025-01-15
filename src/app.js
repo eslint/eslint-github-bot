a40e36dd046f2726d2299180f4467fd8d67c59ce
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2025-01-15 12:43:12 -0500

    fix: Add npm version and fix app setup

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@
 // Requirements
 //-----------------------------------------------------------------------------
 
-const { Probot, run } = require("probot");
+const { run } = require("probot");
 const plugins = require("./plugins");
 
 //-----------------------------------------------------------------------------
@@ -17,32 +17,10 @@ const plugins = require("./plugins");
 //-----------------------------------------------------------------------------
 
 /** @typedef {import("probot").Probot} Probot */
-/** @typedef {import("probot").Context<any>} ProbotContext */
-/** @typedef {import("probot").ProbotOctokit} ProbotOctokit */
 
 //-----------------------------------------------------------------------------
 // Main
 //-----------------------------------------------------------------------------
-
-if (!process.env.SECRET) {
-    throw new Error("Missing 'SECRET' environment variable");
-}
-
-if (!process.env.PRIVATE_KEY) {
-    throw new Error("Missing 'PRIVATE_KEY' environment variable");
-}
-
-if (!process.env.APP_ID) {
-    throw new Error("Missing 'APP_ID' environment variable");
-}
-
-const port = process.env.PORT || 8000;
-const app = new Probot({
-    privateKey: process.env.PRIVATE_KEY,
-    appId: process.env.APP_ID,
-    secret: process.env.SECRET,
-    port
-});
 
 const enabledPlugins = new Set([
     "commitMessage",
@@ -52,10 +30,16 @@ const enabledPlugins = new Set([
     "wip"
 ]);
 
-// load all the enabled plugins from inside plugins folder
-Object.keys(plugins)
-    .filter(pluginId => enabledPlugins.has(pluginId))
-    .forEach(pluginId => app.load(plugins[pluginId]));
+/**
+ * Assign the plugins to the robot.
+ * @param {Probot} robot The Probot instance.
+ * @returns {void}
+ */
+function appFn(robot) {
+    Object.keys(plugins)
+        .filter(pluginId => enabledPlugins.has(pluginId))
+        .forEach(pluginId => plugins[pluginId](robot));
+}
 
 // start the server
-run(app);
+run(appFn);
