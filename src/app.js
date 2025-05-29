da839b4a6f56656c36fb539b826430b7ee9e7247
xbinaryx <xbinaryx68@gmail.com>
2025-05-29 18:16:03 +0300

    feat: automatically assign issues to users willing to submit PRs (#219)
    * feat: automatically assign issues to users willing to submit PRs
    
    * fix test

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ const plugins = require("./plugins");
 //-----------------------------------------------------------------------------
 
 const enabledPlugins = new Set([
+    "autoAssign",
     "commitMessage",
     "needsInfo",
     "recurringIssues",
---
+++ src/plugins/auto-assign/index.js
@@ -0,0 +1,54 @@
+/**
+ * @fileoverview Automatically assigns issues to users who have indicated they're willing to submit a PR
+ * @author xbinaryx
+ */
+
+"use strict";
+
+//-----------------------------------------------------------------------------
+// Type Definitions
+//-----------------------------------------------------------------------------
+
+/** @typedef {import("probot").Context} ProbotContext */
+
+//-----------------------------------------------------------------------------
+// Helpers
+//-----------------------------------------------------------------------------
+
+/**
+ * Checks if the issue body contains text indicating the user is willing to submit a PR
+ * @param {string} body The issue body text
+ * @returns {boolean} True if the user indicated they're willing to submit a PR
+ * @private
+ */
+function isWillingToSubmitPR(body) {
+    return body
+        .toLowerCase()
+        .includes(
+            "- [x] i am willing to submit a pull request to implement this change."
+        );
+}
+
+/**
+ * Handler for issue opened event
+ * @param {ProbotContext} context probot context object
+ * @returns {Promise<void>} promise
+ * @private
+ */
+async function issueOpenedHandler(context) {
+    const { payload } = context;
+
+    if (!isWillingToSubmitPR(payload.issue.body)) {
+        return;
+    }
+
+    await context.octokit.issues.addAssignees(
+        context.issue({
+            assignees: [payload.issue.user.login],
+        })
+    );
+}
+
+module.exports = (robot) => {
+    robot.on("issues.opened", issueOpenedHandler);
+};
